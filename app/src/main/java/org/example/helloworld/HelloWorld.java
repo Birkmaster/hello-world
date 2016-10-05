ff93d6af12cb216d9f480c32f0aef069384ea4cf
Christoph Birkner <birkmaster@hotmail.com>
2016-10-05 19:06:46 +0200

    initial commit

---
+++ app/src/main/java/org/example/helloworld/HelloWorld.java
@@ -0,0 +1,27 @@
+package org.example.helloworld;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+          }
+
+}
+
