914111c1898258bb9eac562cd9d1468ef766e424
Christoph Birkner <birkmaster@hotmail.com>
2016-10-16 16:10:21 +0200

    staged all 2nd commit

--- app/src/main/java/org/example/helloworld/HelloWorld.java
+++ app/src/main/java/org/example/helloworld/HelloWorld.java
@@ -6,9 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HelloWorld extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +22,14 @@ public class HelloWorld extends AppCompatActivity {
         Button buttonnews = (Button) findViewById(R.id.buttonnews);
         buttonnews.setOnClickListener(new View.OnClickListener() {
                                       public void onClick(View v) {
-                                          Context context = getApplicationContext();
-                                          CharSequence text = "Hello news!";
-                                          int duration = Toast.LENGTH_SHORT;
+                Context context = getApplicationContext();
+                CharSequence text = "Hello news!";
+                int duration = Toast.LENGTH_SHORT;
 
-                                          Toast toast = Toast.makeText(context, text, duration);
-                                          toast.show();
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
                                       }
-                                  });
+        });
         Button buttonradio = (Button) findViewById(R.id.buttonradio);
         buttonradio.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -48,5 +53,6 @@ public class HelloWorld extends AppCompatActivity {
             }
         });
     }
+
 }
 
