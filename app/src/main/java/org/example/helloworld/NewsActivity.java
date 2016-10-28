efade4a9964943125d1e4305fc877f29e4e30c32
Christoph Birkner <birkmaster@hotmail.com>
2016-10-28 21:21:16 +0200

    news_activity bug repariert

--- app/src/main/java/org/example/helloworld/NewsActivity.java
+++ app/src/main/java/org/example/helloworld/NewsActivity.java
@@ -1,13 +1,7 @@
 package org.example.helloworld;
 
-import android.content.Context;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
-import android.widget.Toast;
-
+import android.os.Bundle;
 
 public class NewsActivity extends AppCompatActivity {
 
