3b0cf1719f60af29281a7d6f1e7a7d48dd24279c
Christoph Birkner <birkmaster@hotmail.com>
2016-11-16 22:39:10 +0100

    RssAusgabe

--- app/src/main/java/org/example/helloworld/OrfNewsRssParser.java
+++ app/src/main/java/org/example/helloworld/OrfNewsRssParser.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by Birkmaster on 09.11.2016.
  */
@@ -20,6 +19,90 @@ import java.util.List;
 public class OrfNewsRssParser {
     // We don't use namespaces
     private static final String ns = null;
+
+    public static class Item {
+        public final String title;
+        public final String link;
+
+        private Item(String title, String link) {
+            this.title = title;
+            this.link = link;
+        }
+    }
+
+    // Parses the contents of an entry. If it encounters a title or link tag, hands them off
+// to their respective "read" methods for processing. Otherwise, skips the tag.
+    private Item readItem(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "item");
+        String title = null;
+        String link = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("title")) {
+                title = readTitle(parser);
+            } else if (name.equals("link")) {
+                link = readLink(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Item(title, link);
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+    // Processes title tags in the feed.
+    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "title");
+        return title;
+    }
+
+    // Processes link tags in the feed.
+    private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String link = "";
+        parser.require(XmlPullParser.START_TAG, ns, "link");
+        String tag = parser.getName();
+        String relType = parser.getAttributeValue(null, "rel");
+        if (tag.equals("link")) {
+            if (relType.equals("alternate")){
+                link = parser.getAttributeValue(null, "href");
+                parser.nextTag();
+            }
+        }
+        parser.require(XmlPullParser.END_TAG, ns, "link");
+        return link;
+    }
+
+    // For the tag title extract the text value.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
 
     public List parse(String xml) throws XmlPullParserException, IOException {
         XmlPullParser parser = Xml.newPullParser();
@@ -31,7 +114,7 @@ public class OrfNewsRssParser {
     }
 
     private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-        List entries = new ArrayList();
+        List items = new ArrayList();
 
         parser.require(XmlPullParser.START_TAG, ns, "feed");
         while (parser.next() != XmlPullParser.END_TAG) {
@@ -40,12 +123,14 @@ public class OrfNewsRssParser {
             }
             String name = parser.getName();
             // Starts by looking for the entry tag
-            if (name.equals("entry")) {
-                entries.add(readEntry(parser));
+            if (name.equals("item")) {
+                items.add(readItem(parser));
             } else {
                 skip(parser);
             }
         }
-        return entries;
+        return items;
     }
+
+
 }
